298aff5720b6b90df7088c3cb6e3adeb4bbbbc92
sarwarali847 <sarwarali847@gmail.com>
2021-09-16 12:44:03 +0530

    My First Commit

---
+++ GitHubTest/src/sarwar/test.java
@@ -0,0 +1,15 @@
+package sarwar;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		if(a==10)
+			System.out.print("Value of a is 10");
+		else
+			System.out.print("Value of a is not found");
+
+	}
+
+}
